7e21a5942be5797d45a39616521ad56a7fb0b518
janghyunmoon <abc9529@naver.com>
2019-04-05 13:05:13 +0900

    한글 깨짐 수정

--- pickmatch/src/main/java/com/kh/pickmatch/controller/MemberController.java
+++ pickmatch/src/main/java/com/kh/pickmatch/controller/MemberController.java
@@ -16,82 +16,82 @@ import com.kh.pickmatch.model.vo.Member;
 
 @Controller
 public class MemberController {
-	
-	private Logger logger = LoggerFactory.getLogger(MemberController.class);
-	
-	@Autowired
-	private MemberService service;
-	
-	@Autowired
-	private BCryptPasswordEncoder bcEncoder;
-	
-/*	@RequestMapping("/member/checkId.do")
-	public ModelAndView checkId(String userId) throws UnsupportedEncodingException
-	{
-		ModelAndView mv = new ModelAndView();
-		Member m = new Member();
-		mv.setViewName("member/");
-		return mv;
-	}*/
+   
+   private Logger logger = LoggerFactory.getLogger(MemberController.class);
+   
+   @Autowired
+   private MemberService service;
+   
+   @Autowired
+   private BCryptPasswordEncoder bcEncoder;
+   
+/*   @RequestMapping("/member/checkId.do")
+   public ModelAndView checkId(String userId) throws UnsupportedEncodingException
+   {
+      ModelAndView mv = new ModelAndView();
+      Member m = new Member();
+      mv.setViewName("member/");
+      return mv;
+   }*/
 
-	@RequestMapping("/member/login.do")
-	public String login(Member m, Model model, HttpSession session)
-	{
-		logger.debug(""+m);
-		logger.debug(""+session);
-		Member result = service.selectOne(m);
-		String msg="";
-		String loc="/";
-		if(result != null)
-		{
-			if(bcEncoder.matches(m.getPassword(), result.getPassword()))
-			{
-				session.setAttribute("loggedMember", result);
-				return "redirect:/";
-			}
-			else
-			{
-				msg = "?⑥뒪?뚮뱶媛 ?쇱튂?섏? ?딆뒿?덈떎.";
-			}
-		}
-		else
-		{
-			msg = "議댁옱?섏? ?딅뒗 ?꾩씠?붿엯?덈떎.";
-		}
-		model.addAttribute("msg",msg);
-		model.addAttribute("loc",loc);
-		return "common/msg";
-	}
-	
-	@RequestMapping("/member/logout.do")
-	public String logout(HttpSession session)
-	{
-		session.invalidate();
-		return "redirect:/";
-	}
-	
-	@RequestMapping("/member/memberEnroll.do")
-	public String memberEnroll(Member m, Model model)
-	{
-		String rawPw = m.getPassword();
-		String enPw = bcEncoder.encode(rawPw);
-		m.setPassword(enPw);
-		
-		String msg = "";
-		String loc = "/";
-		
-		int result = service.insertMember(m);
-		if(result == 0)
-		{
-			msg = "?뚯썝媛?낆뿉 ?ㅽ뙣?섏??듬땲??";			
-		}
-		else
-		{
-			msg = m.getMemberId()+"???뚯썝媛?낅릺?덉뒿?덈떎.";
-		}
-		model.addAttribute("msg",msg);
-		model.addAttribute("loc",loc);
-		return "common/msg";
-	}
-	
+   @RequestMapping("/member/login.do")
+   public String login(Member m, Model model, HttpSession session)
+   {
+      logger.debug(""+m);
+      logger.debug(""+session);
+      Member result = service.selectOne(m);
+      String msg="";
+      String loc="/";
+      if(result != null)
+      {
+         if(bcEncoder.matches(m.getPassword(), result.getPassword()))
+         {
+            session.setAttribute("loggedMember", result);
+            return "redirect:/";
+         }
+         else
+         {
+            msg = "패스워드가 일치하지 않습니다.";
+         }
+      }
+      else
+      {
+         msg = "존재하지 않는 아이디입니다.";
+      }
+      model.addAttribute("msg",msg);
+      model.addAttribute("loc",loc);
+      return "common/msg";
+   }
+   
+   @RequestMapping("/member/logout.do")
+   public String logout(HttpSession session)
+   {
+      session.invalidate();
+      return "redirect:/";
+   }
+   
+   @RequestMapping("/member/memberEnroll.do")
+   public String memberEnroll(Member m, Model model)
+   {
+      String rawPw = m.getPassword();
+      String enPw = bcEncoder.encode(rawPw);
+      m.setPassword(enPw);
+      
+      String msg = "";
+      String loc = "/";
+      
+      int result = service.insertMember(m);
+      if(result == 0)
+      {
+         msg = "회원가입에 실패하였습니다.";         
+      }
+      else
+      {
+         msg = m.getMemberId()+"님 회원가입되었습니다.";
+      }
+      model.addAttribute("msg",msg);
+      model.addAttribute("loc",loc);
+      return "common/msg";
+   }
+   
 }
--- pickmatch/src/main/java/com/kh/pickmatch/controller/TeamController.java
+++ pickmatch/src/main/java/com/kh/pickmatch/controller/TeamController.java
@@ -20,202 +20,212 @@ import com.kh.pickmatch.model.service.TeamService;
 import com.kh.pickmatch.model.vo.Member;
 import com.kh.pickmatch.model.vo.MoneyHistory;
 import com.kh.pickmatch.model.vo.TeamBoard;
+import com.kh.pickmatch.model.vo.TeamNotice;
 import com.kh.pickmatch.model.vo.TeamOperationAccount;
 
 @Controller
 public class TeamController {
 
-	@Autowired
-	TeamService service;
-	
-	@RequestMapping("/team/teamOperationInfo")
-	public ModelAndView teamOperationInfo(HttpSession session, String selectDate) {
-		
-		ModelAndView mv = new ModelAndView();
-		String msg = "";
-		String loc = "";
-		String teamName = "";
-		Member m = null;
-		String memberId = "";
-		
-		if(session.getAttribute("loggedMember") == null) {
-			msg = "濡쒓렇?????댁슜媛?ν빀?덈떎.";
-			loc = "/";
-			
-			mv.setViewName("common/msg");
-			mv.addObject("msg", msg);
-			mv.addObject("loc", loc);
-			return mv;
-			
-		} else {
-			m = (Member)session.getAttribute("loggedMember");
-			memberId = m.getMemberId();
-			teamName = service.selectTeamOne(memberId);
-			if(teamName == null) {
-				msg = "?뚯냽??????놁뒿?덈떎.";
-				loc = "/";
-				
-				mv.setViewName("common/msg");
-				mv.addObject("msg", msg);
-				mv.addObject("loc", loc);
-				return mv;
-				
-			}
-		}
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
-		Date date = new Date();
-		String currentDate = "";
-		
-		if(selectDate == null) currentDate = sdf.format(date);
-		else currentDate = selectDate;
-		
-		currentDate += "-01";
-		
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("currentDate", currentDate);
-		map.put("teamName", teamName);
-		
-		List<Map<String, String>> listTMember = service.selectTMemberList(map);
-		List<Map<String, Object>> listMHistory = service.selectMoneyHistoryList(teamName);
-		
-		TeamOperationAccount account = service.selectAccountOne(teamName);
-		
-		mv.setViewName("team/teamOperationInfo");
-		mv.addObject("currentDate", currentDate);
-		mv.addObject("memberId", memberId);
-		mv.addObject("teamName", teamName);
-		mv.addObject("listTMember", listTMember);
-		mv.addObject("listMHistory", listMHistory);
-		mv.addObject("account", account);
-		
-		return mv;
-		
-	}
-	
-	@RequestMapping("/team/teamaccountRevise")
-	public String teamAccountRevise(TeamOperationAccount toAccount, Model model) {
-		
-		int result = service.updateTeamAccount(toAccount);
-		String msg = "";
-		String loc = "/team/teamOperationInfo";
-		
-		if(result > 0) msg = "怨꾩쥖?뺣낫媛 ?섏젙?꾨즺 ?섏뿀?듬땲??";
-		else msg = "怨꾩쥖 ?섏젙以묒뿉 ?ㅻ쪟媛 諛쒖깮?섏??듬땲??";
-		
-		model.addAttribute("msg", msg);
-		model.addAttribute("loc", loc);
-		return "common/msg";
-	}
-	
-	@RequestMapping("/team/teamaccountEnroll")
-	public String teamAccountEnroll(TeamOperationAccount toAccount, Model model) {
-		
-		int result = service.insertTeamAccount(toAccount);
-		String msg = "";
-		String loc = "/team/teamOperationInfo";
-		
-		if(result > 0) msg = "怨꾩쥖媛 ?깅줉 ?섏뿀?듬땲??";
-		else msg = "怨꾩쥖 ?깅줉以묒뿉 ?ㅻ쪟媛 諛쒖깮?섏??듬땲??";
-		
-		model.addAttribute("msg", msg);
-		model.addAttribute("loc", loc);
-		return "common/msg";
-	}
-	
-	@RequestMapping("/team/MoneyHistoryEndroll")
-	public String MoneyHistoryEndroll(MoneyHistory mHistory, Model model) {
-		
-		
-		int result = service.insertMHistory(mHistory);
-		
-		String msg = "";
-		String loc = "/team/teamOperationInfo";
+   @Autowired
+   TeamService service;
+   
+   @RequestMapping("/team/teamOperationInfo")
+   public ModelAndView teamOperationInfo(HttpSession session, String selectDate) {
+      
+      ModelAndView mv = new ModelAndView();
+      String msg = "";
+      String loc = "";
+      String teamName = "";
+      Member m = null;
+      String memberId = "";
+      
+      if(session.getAttribute("loggedMember") == null) {
+         msg = "로그인 후 이용가능합니다.";
+         loc = "/";
+         
+         mv.setViewName("common/msg");
+         mv.addObject("msg", msg);
+         mv.addObject("loc", loc);
+         return mv;
+         
+      } else {
+         m = (Member)session.getAttribute("loggedMember");
+         memberId = m.getMemberId();
+         teamName = service.selectTeamOne(memberId);
+         if(teamName == null) {
+            msg = "소속된 팀이 없습니다.";
+            loc = "/";
+            
+            mv.setViewName("common/msg");
+            mv.addObject("msg", msg);
+            mv.addObject("loc", loc);
+            return mv;
+            
+         }
+      }
+      
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+      Date date = new Date();
+      String currentDate = "";
+      
+      if(selectDate == null) currentDate = sdf.format(date);
+      else currentDate = selectDate;
+      
+      currentDate += "-01";
+      
+      Map<String, String> map = new HashMap<String, String>();
+      map.put("currentDate", currentDate);
+      map.put("teamName", teamName);
+      
+      List<Map<String, String>> listTMember = service.selectTMemberList(map);
+      List<Map<String, Object>> listMHistory = service.selectMoneyHistoryList(teamName);
+      
+      TeamOperationAccount account = service.selectAccountOne(teamName);
+      
+      mv.setViewName("team/teamOperationInfo");
+      mv.addObject("currentDate", currentDate);
+      mv.addObject("memberId", memberId);
+      mv.addObject("teamName", teamName);
+      mv.addObject("listTMember", listTMember);
+      mv.addObject("listMHistory", listMHistory);
+      mv.addObject("account", account);
+      
+      return mv;
+      
+   }
+   
+   @RequestMapping("/team/teamaccountRevise")
+   public String teamAccountRevise(TeamOperationAccount toAccount, Model model) {
+      
+      int result = service.updateTeamAccount(toAccount);
+      String msg = "";
+      String loc = "/team/teamOperationInfo";
+      
+      if(result > 0) msg = "계좌정보가 수정완료 되었습니다!";
+      else msg = "계좌 수정중에 오류가 발생하였습니다!";
+      
+      model.addAttribute("msg", msg);
+      model.addAttribute("loc", loc);
+      return "common/msg";
+   }
+   
+   @RequestMapping("/team/teamaccountEnroll")
+   public String teamAccountEnroll(TeamOperationAccount toAccount, Model model) {
+      
+      int result = service.insertTeamAccount(toAccount);
+      String msg = "";
+      String loc = "/team/teamOperationInfo";
+      
+      if(result > 0) msg = "계좌가 등록 되었습니다!";
+      else msg = "계좌 등록중에 오류가 발생하였습니다!";
+      
+      model.addAttribute("msg", msg);
+      model.addAttribute("loc", loc);
+      return "common/msg";
+   }
+   
+   @RequestMapping("/team/MoneyHistoryEndroll")
+   public String MoneyHistoryEndroll(MoneyHistory mHistory, Model model) {
+      
+      
+      int result = service.insertMHistory(mHistory);
+      
+      String msg = "";
+      String loc = "/team/teamOperationInfo";
 
-		if(result > 0) msg = "?댁뿭?쒓? ?묒꽦?섏뿀?듬땲??";
-		else msg = "?댁뿭???묒꽦以??ㅻ쪟諛쒖깮!";
-		
-		model.addAttribute("msg", msg);
-		model.addAttribute("loc", loc);
-		return "common/msg";
-	}
-	
-	@RequestMapping("/team.do")
-	public String teaminfo() {
-		return "Team/teaminfo";
-		
-	}
-	
-	@RequestMapping("/teammercenary.do")
-	public String teammercenaryinfo() {
-		return "Team/teammercenary";
-		
-	}
-	
-	@RequestMapping("/teamcreate.do")
-	public String teamcreate (){
-		return "Team/teamcreate";
-		
-	}
-	
-	@RequestMapping("/teamnotice.do")
-	public String teamnotice() {
-		return "Team/teamnotice";
-	}
-	
-	@RequestMapping("/freeboard.do")
-	public ModelAndView teamranking(@RequestParam(value="cPage", required=false, defaultValue="1")int cPage) {
-		int numPerPage = 10;
-		ModelAndView mv = new ModelAndView();
-		List<TeamBoard> list = service.selectList(cPage, numPerPage);
-		int totalList = service.selectCount();
-		
-		System.out.println(list);
-		
-		mv.addObject("list", list);
-		mv.addObject("totalList", totalList);
-		System.out.println(totalList);
-		mv.addObject("pageBar", PageBarFactory.getPageBar(totalList, cPage, numPerPage, "/pickmatch/Team/freeboard"));
-		mv.setViewName("Team/freeboard");
-		return mv;
-	}
-		
-		
-	
-	
-	@RequestMapping("/teamranking.do")
-	public String teamranking(){
-		return "Team/teamranking";
-	}
-	
-	@RequestMapping("/mercenaryranking.do")
-	public String mercenaryranking() {
-		return "Team/mercenaryranking";
-	}
-	
-	@RequestMapping("/Team/freeboardWrite")
-	public String freeboardWrite() {
-		return "Team/freeboardWrite";
-	}
-	
-	@RequestMapping("/Team/teamnoticeWrite")
-	public String teamnoticeWrite(){
-		return "Team/teamnoticeWrite";
-	}
-	
-	@RequestMapping("/freeboardView.do")
-	public String freeboardView() {
-		return "Team/teamboardView";
-	}
-	
-	@RequestMapping("/team/teamView.do")
-	public ModelAndView selectOne(@RequestParam(value="boardNo",defaultValue="1") int boardNo) {
-		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("teamboard", service.selectTeamBoard(boardNo));
-		mv.addObject("attachmentList", service.selectAttachment(boardNo));
-		mv.setViewName("Team/teamboardView");
-		return mv;
-	}
-	
+      if(result > 0) msg = "내역서가 작성되었습니다.";
+      else msg = "내역서 작성중 오류발생!";
+      
+      model.addAttribute("msg", msg);
+      model.addAttribute("loc", loc);
+      return "common/msg";
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   //도원
+   @RequestMapping("/team.do")
+   public String teaminfo() {
+      return "Team/teaminfo";
+      
+   }
+   
+   @RequestMapping("/teammercenary.do")
+   public String teammercenaryinfo() {
+      return "Team/teammercenary";
+      
+   }
+   
+   @RequestMapping("/teamcreate.do")
+   public String teamcreate (){
+      return "Team/teamcreate";
+      
+   }
+   
+   
+   
+   //팀 게시판 리스트보기
+   @RequestMapping("/freeboard.do")
+   public ModelAndView freeboard(@RequestParam(value="cPage", required=false, defaultValue="1")int cPage) {
+      int numPerPage = 10;
+      ModelAndView mv = new ModelAndView();
+      List<TeamBoard> list = service.selectList(cPage, numPerPage);
+      int totalList = service.selectCount();
+      
+      System.out.println(list);
+      
+      mv.addObject("list", list);
+      mv.addObject("totalList", totalList);
+      System.out.println(totalList);
+      mv.addObject("pageBar", PageBarFactory.getPageBar(totalList, cPage, numPerPage, "/pickmatch/Team/freeboard.do"));
+      mv.setViewName("Team/freeboard");
+      return mv;
+   }
+      
+      
+   
+   
+   @RequestMapping("/teamranking.do")
+   public String teamranking(){
+      return "Team/teamranking";
+   }
+   
+   @RequestMapping("/mercenaryranking.do")
+   public String mercenaryranking() {
+      return "Team/mercenaryranking";
+   }
+   
+   //팀 게시판 글쓰기
+   @RequestMapping("/Team/freeboardWrite")
+   public String freeboardWrite() {
+      return "Team/freeboardWrite";
+   }
+   
+   //팀 공지사항 글쓰기
+   @RequestMapping("/Team/teamnoticeWrite")
+   public String teamnoticeWrite(){
+      return "Team/teamnoticeWrite";
+   }
+   
+   @RequestMapping("/freeboardView.do")
+   public String freeboardView() {
+      return "Team/teamboardView";
+   }
+   
+   //팀 게시판 상세보기
+   @RequestMapping("/team/teamView.do")
+   public ModelAndView selectOne(@RequestParam(value="boardNo",defaultValue="1") int boardNo) {
+      
+      ModelAndView mv = new ModelAndView();
+      mv.addObject("teamboard", service.selectTeamBoard(boardNo));
+      mv.addObject("attachmentList", service.selectAttachment(boardNo));
+      mv.setViewName("Team/teamboardView");
+      return mv;
+   }
+   
 }
